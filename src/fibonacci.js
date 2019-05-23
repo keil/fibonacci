de05bdb32d6b5af13912f01efe11fa549749c828
System Administrator <root@Matthiass-MacBook-Pro-3.local>
2019-05-23 15:58:31 +0200

    add value check

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -3,13 +3,14 @@ let fibonacci = (function() {
   let map = new Map();
 
   let fibonacci = function(n) {
-    // TODO test for numbers
     if(n==0) return 0;
     else if(n==1) return 1;
     else return calculate(n-1)+calculate(n-2);
   }
 
   let calculate = function(n) {
+    if(typeof n != 'number') throw new Error("Invalid Input: Not a value of type number.");
+    if(n<0) throw new Error("Invalid Input: Not a value greater or equals 0.");
     if(!map.has(n)) map.set(n, fibonacci(n));
     return map.get(n);
   }
